6d652cb3c9078bcd13e79d33a641a27612e0e780
miguel <miguel@miguel-VPCEA36FM.(none)>
2016-03-29 09:33:27 -0300

    Trying to put multiple projects into one repo...

---
+++ Calculator/src/ui/MainFrame.java
@@ -0,0 +1,44 @@
+package ui;
+
+import engine.CalculatorEngine;
+
+public class MainFrame {
+	public static void main(String [] args) {
+		CalculatorEngine c = new CalculatorEngine();
+		
+		c.digit(1);
+		c.digit(3);
+		c.add();
+		c.digit(1);
+		c.digit(1);
+		c.compute();
+		System.out.println(c.display());
+		
+		c.subtract();
+		c.digit(4);
+		c.compute();
+		System.out.println(c.display());
+		
+		c.multiply();
+		c.digit(3);
+		c.compute();
+		System.out.println(c.display());
+		
+		c.divide();
+		c.digit(1);
+		c.digit(5);
+		c.compute();
+		System.out.println(c.display());
+		
+		c.clear();
+		System.out.println(c.display());
+		
+		c.digit(1);
+		c.digit(3);
+		c.add();
+		c.digit(1);
+		c.digit(1);
+		c.compute();
+		System.out.println(c.display());
+	}
+}
