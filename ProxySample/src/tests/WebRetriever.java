1a6e48823a4f6d91f3af9b37517659c9d918d92e
miguel <miguel@miguel-VPCEA36FM.(none)>
2016-04-27 17:02:05 -0300

    Modifying WebRetriever class

--- ProxySample/src/tests/WebRetriever.java
+++ ProxySample/src/tests/WebRetriever.java
@@ -13,39 +13,29 @@ class WebRetriever {
 			os = soc.getOutputStream();
 			is = soc.getInputStream();
 	}
-	
-	WebRetriever(Socket s) throws IOException {
-		soc = s;
-		os = soc.getOutputStream();
-		is = soc.getInputStream();
-	}
 
-	public void request(String path) {
-		try {
-			String message = "GET " + path + "\n\n";
-			os.write(message.getBytes());
-			os.flush();
-		} catch (IOException e) {
-			System.err.println("Error in HTTP request");
-		}
-	}
-	public void request(WebProxy p, String path) {
-		p.requestToServer(path);
+	public void request(String path, String host, int hostPort) {
+		PrintWriter pw = new PrintWriter(os);
+		pw.println("GET "+path+" HTTP/1.1\r");
+		pw.println("Host: "+host+":"+hostPort+"\r");
+		pw.println("Accept: */*\r\n Accept-Encoding: gzip, deflate\r\n "
+				+ "User-Agent: runscope/0.1\r\n\r");
+		pw.flush();
 	}
 	
-	public void getResponse() {
+	public String getResponse() {
 		int c;
+		String response = "";
+		System.out.println("HTTP RESPONSE:");
 		try {
 			while ((c = is.read()) != -1)
 				System.out.print((char) c) ;
+			response += (char) c;
 		} catch (IOException e) {
 			System.err.println("IOException in reading from " +
 					"Web server");
 		}
-	}
-	
-	public void getResponse(WebProxy p) {
-		p.getResponseFromServer();
+		return response;
 	}
 	
 	public void close() {
