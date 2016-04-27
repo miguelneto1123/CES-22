2836e8dbbe71dcb95da84342cad01db40a55d2d0
miguel <miguel@miguel-VPCEA36FM.(none)>
2016-04-27 10:46:07 -0300

    Completing MyProxy class

--- ProxySample/src/proxy/MyProxy.java
+++ ProxySample/src/proxy/MyProxy.java
@@ -4,5 +4,31 @@ import java.net.*;
 import java.io.*;
 
 public class MyProxy {
+	private ServerSocket client;
+	private Socket server;
 	
+	public void go(int serverPort, int clientPort) {
+		try {
+			client = new ServerSocket(clientPort);
+			
+			while (true) {	
+				server = new Socket("localhost", serverPort);
+				
+				Socket socClient = client.accept();
+				
+				InputStreamReader inputServer =
+						new InputStreamReader(server.getInputStream());
+				BufferedReader buffer = new BufferedReader(inputServer);
+				PrintWriter pw = new PrintWriter(socClient.getOutputStream());
+				
+				pw.println("From proxy: " + buffer.readLine());
+				
+				pw.close();
+				buffer.close();
+			}
+			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
 }
